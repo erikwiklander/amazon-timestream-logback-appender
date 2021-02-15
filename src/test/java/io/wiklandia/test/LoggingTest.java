b8d9ee52e1df1f473ec8cc6d77561137e0108f20
Erik Wiklander <erik.wiklander@str8lines.com>
2021-02-15 08:54:20 -0600

    Added a test

---
+++ src/test/java/io/wiklandia/test/LoggingTest.java
@@ -0,0 +1,16 @@
+package io.wiklandia.test;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class LoggingTest {
+
+    @Test
+    void test() {
+        System.out.println("yey");
+    }
+
+
+}
